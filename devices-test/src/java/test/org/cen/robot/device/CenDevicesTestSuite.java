4c86d926e198a696240e6d3c36215d8f406d71db
Stephane Vanacker <svanacker@talentsoft.com>
2014-05-05 21:42:18 +0200

    Initial decomposition into tests - projects

---
+++ devices-test/src/java/test/org/cen/robot/device/CenDevicesTestSuite.java
@@ -0,0 +1,12 @@
+package org.cen.robot.device;
+
+import org.cen.robot.device.lcd.LcdDeviceTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses(value = { LcdDeviceTest.class })
+public class CenDevicesTestSuite {
+
+}
---
+++ math-test/src/java/test/org/cen/math/CenMathTestSuite.java
@@ -0,0 +1,11 @@
+package org.cen.math;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses(value = { MathUtilsTest.class })
+public class CenMathTestSuite {
+
+}
